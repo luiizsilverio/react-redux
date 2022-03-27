1b9195f64c2fa579951e2613de2914171ae37341
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-27 16:35:26 -0300

    API fake

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import api from '../services/api'
+import { IProduct } from '../store/modules/cart/types'
 
 const Catalog: React.FC = () => {
-    const catalog = useSelector(state => state)
-  
-    console.log(catalog)
+    const [catalog, setCatalog] = useState<IProduct[]>([])
 
-    return (        
+    useEffect(() => {
+      api.get('products').then(response => {
+        setCatalog(response.data)
+      })
+    })
+    return (
+      <main>
         <h1>Catalog</h1>
+        {
+          catalog.map(product => (
+            <article key={product.id}>
+              <strong>{product.title}</strong> {" - "}
+              <span>R$ {product.price.toFixed(2)}</span> {" "}
+              <button type="button">Comprar</button>
+            </article>
+          ))
+        }
+      </main>
     )
 }
 
